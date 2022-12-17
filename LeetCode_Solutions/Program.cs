a1e0732bb7f2817bd2a44128a12d4b8f87e744dc
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2022-12-17 21:50:29 +0100

    Add project files.

---
+++ LeetCode_Solutions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int RomanToInt(string s)
+        {
+            var dictionary = new Dictionary<char, int>();
+            dictionary.Add('I', 1);
+            dictionary.Add('V', 5);
+            dictionary.Add('X', 10);
+            dictionary.Add('L', 50);
+            dictionary.Add('C', 100);
+            dictionary.Add('D', 500);
+            dictionary.Add('M', 1000);
+
+            int solution = 0;
+
+            s = s.Replace("IV", "IIII")
+                .Replace("IX", "VIIII")
+                .Replace("XL", "XXXX")
+                .Replace("XC", "LXXXX")
+                .Replace("CD", "CCCC")
+                .Replace("CM", "DCCCC");
+
+            foreach (char c in s)
+            {
+                solution += dictionary[c];
+            }
+            return solution;
+        }
+    }
+}
---
+++ Two_Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 11, 15,18,24,2, 7 };
+            int target = 9;
+
+
+            foreach (var item in TwoSum(nums,target))
+            {
+                Console.WriteLine(item);
+            }
+            
+            
+
+            Console.ReadKey();
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            int[] arr = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        arr[0] = i;
+                        arr[1] = j;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+}
